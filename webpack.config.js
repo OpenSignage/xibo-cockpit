1dc5d68d028c77da27da693f54902138a95c60c8
MiuraMasataka <109357892+MiuraMasataka@users.noreply.github.com>
2025-05-20 01:12:26 +0900

    update files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'client.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
+    }),
+  ],
+  devtool: 'source-map',
+}; 
